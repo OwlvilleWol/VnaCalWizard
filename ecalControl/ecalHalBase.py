c3e13dccdb8c2e4780fae16177f20f96d31a0cdc
Bagojfalvibagoj <bagojfalvibagoj@gmail.com>
2024-05-11 10:19:51 -0400

    small fix added environment.yaml added doc schematic images for hardware examples

--- ecalControl/ecalHalBase.py
+++ ecalControl/ecalHalBase.py
@@ -1,15 +1,7 @@
+from . import EcalHalAbc
 
-class ECalHal:
 
-    _MUX_ADDR_FLASH_ADDR_0_7 = 2
-    _MUX_ADDR_FLASH_ADDR_8_15 = 3
-    _MUX_ADDR_FLASH_ADDR_16_17 = 4
-
-    _MUX_ADDR_GATES_1_8 = 0
-    _MUX_ADDR_GATES_9_16 = 1
-
-    _MUX_ADDR_FLASH_DATA = 7
-
+class ECalHal(EcalHalAbc):
 
     def __init__(self) -> None:
         pass 

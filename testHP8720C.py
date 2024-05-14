102207efe700656b9b73e3f8db937ec44c79e219
Bagojfalvibagoj <bagojfalvibagoj@gmail.com>
2024-05-14 00:01:40 -0400

    hp8720 operator prompt done

--- testHP8720C.py
+++ testHP8720C.py
@@ -1,10 +1,17 @@
 from hp8720c import HP8720C
 
 
+
 vna = HP8720C("GPIB0::16::INSTR", reset=False, backend="@ivi")
 print(vna.frequency)
 
 vna.npoints = 201
-network = vna.get_network([1])
+#network = vna.get_network([1])
+#print(network)
 
-print(network)
+
+if (vna.operatorPrompt != None):
+    print(vna.operatorPrompt("Have you connected the ECal yet you halfwit?", {1 : ("Yes","y"), 2: ("No", "n"), 8: ("Abort", "a")}))
+
+
+
